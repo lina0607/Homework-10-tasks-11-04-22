2cf4c16adfe05b7f9b892cb85c822e946fa95a46
Lina <lnzitniak@gmail.com>
2022-04-22 17:00:56 +0300

    Completing task10

--- src/com/company/task10.java
+++ src/com/company/task10.java
@@ -5,26 +5,40 @@ import java.util.Scanner;
 
 public class task10 {
 
+
     public void calculate() {
 
         Scanner scanner = new Scanner(System.in);
 
         int elements_number;
-        System.out.println("Provide an integer for multiplication:"); //we should multiply all elements for this number
+        System.out.println("Provide an integer for multiplication:"); //we should multiply all elements with this number
         int provided_integer = scanner.nextInt();
 
         System.out.println("Enter how many elements to add to the array:");  //how long will the array be
         elements_number = scanner.nextInt();
-        int a[] = new int[elements_number];
+        int array [] = new int[elements_number];
+        int[] arr2 = new int[array.length - 2];
 
         System.out.println("Input numbers:");           //numbers that will create the array
 
 
         for (int i = 0; i < elements_number; i++) {     //multiplication of each number of the array list
-            a[i] = scanner.nextInt();
-            a[i] = a[i] * provided_integer;
+            array[i] = scanner.nextInt();
+            //      array[i] = array[i] * provided_integer;
         }
-            System.out.println("New Array with the multiplied values is: " + Arrays.toString(a));
+
+        int value = provided_integer;
+
+        for(int i = 0; i < array.length; i++) {
+            if (i ==0 || i == array.length - 1) {
+                continue;
+            }
+            //multiply the value of the elements with given value and save result to new array
+            arr2[i - 1] = array[i] * value;
+        }
+        System.out.println(Arrays.toString(arr2));
+
         }
     }
+
 
